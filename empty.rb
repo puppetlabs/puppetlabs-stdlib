de0e7adb66fd00a8b72b7e7746bd9844bca797d3
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-04-26 00:09:26 +0100

    First version.  Simple empty function to use within Puppet DSL.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

---
+++ empty.rb
@@ -0,0 +1,25 @@
+#
+# empty.rb
+#
+
+module Puppet::Parser::Functions
+  newfunction(:empty, :type => :rvalue, :doc => <<-EOS
+    EOS
+  ) do |arguments|
+
+    raise(Puppet::ParseError, "empty(): Wrong number of arguments " +
+      "given (#{arguments.size} for 1)") if arguments.size < 1
+
+    array = arguments[0]
+
+    if not array.is_a?(Array)
+      raise(Puppet::ParseError, 'empty(): Requires an array to work with')
+    end
+
+    result = array.empty?
+
+    return result
+  end
+end
+
+# vim: set ts=2 sw=2 et :
