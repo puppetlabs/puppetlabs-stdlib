c9dcca03b5106a69a39e88ec1c0c53a7ffc121e7
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-04-26 00:14:48 +0100

    First version.  Simple unique function to use within Puppet DSL.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

---
+++ unique.rb
@@ -0,0 +1,25 @@
+#
+# unique.rb
+#
+
+module Puppet::Parser::Functions
+  newfunction(:unique, :type => :rvalue, :doc => <<-EOS
+    EOS
+  ) do |arguments|
+
+    raise(Puppet::ParseError, "unique(): Wrong number of arguments " +
+      "given (#{arguments.size} for 1)") if arguments.size < 1
+
+    array = arguments[0]
+
+    if not array.is_a?(Array)
+      raise(Puppet::ParseError, 'unique(): Requires an array to work with')
+    end
+
+    result = array.uniq
+
+    return result
+  end
+end
+
+# vim: set ts=2 sw=2 et :
