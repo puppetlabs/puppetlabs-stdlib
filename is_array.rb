d495723d6f0b400755c749e68c0a8b8d49570848
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-04-28 03:43:14 +0100

    First version.  Simple is_array function to use within Puppet DSL.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

---
+++ is_array.rb
@@ -0,0 +1,21 @@
+#
+# is_array.rb
+#
+
+module Puppet::Parser::Functions
+  newfunction(:is_array, :type => :rvalue, :doc => <<-EOS
+    EOS
+  ) do |arguments|
+
+    raise(Puppet::ParseError, "is_array(): Wrong number of arguments " +
+      "given (#{arguments.size} for 1)") if arguments.size < 1
+
+    type = arguments[0]
+
+    result = type.is_a?(Array)
+
+    return result
+  end
+end
+
+# vim: set ts=2 sw=2 et :
