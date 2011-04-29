606820a4c90d358b39b32f7f15b25cef496e08f7
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-04-29 04:21:20 +0100

    Added TODO for future reference.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- reverse.rb
+++ reverse.rb
@@ -2,6 +2,8 @@
 # reverse.rb
 #
 
+# TODO(Krzysztof Wilczynski): Support for strings would be nice too ...
+
 module Puppet::Parser::Functions
   newfunction(:reverse, :type => :rvalue, :doc => <<-EOS
     EOS
