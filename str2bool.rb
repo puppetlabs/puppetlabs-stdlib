f3b6b11e85ba42af809c12e50f0172dc582a680a
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-04-29 20:13:27 +0100

    First version.  Simple str2bool function to use within Puppet DSL.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

---
+++ str2bool.rb
@@ -0,0 +1,38 @@
+#
+# str2bool.rb
+#
+
+module Puppet::Parser::Functions
+  newfunction(:str2bool, :type => :rvalue, :doc => <<-EOS
+    EOS
+  ) do |arguments|
+
+    raise(Puppet::ParseError, "str2bool(): Wrong number of arguments " +
+      "given (#{arguments.size} for 1)") if arguments.size < 1
+
+    string = arguments[0]
+
+    if not string.is_a?(String)
+      raise(Puppet::ParseError, 'str2bool(): Requires either a ' +
+        'string to work with')
+    end
+
+    result = case string
+      #
+      # This is how undef looks like in Puppet ...
+      # We yield false in this case.
+      #
+      when /^$/, '' then false
+      when /^(1|t|y|true|yes)$/ then true
+      when /^(0|f|n|false|no)$/ then false
+      # This is not likely to happen ...
+      when /^(undef|undefined)$/ then false
+      else
+        raise(Puppet::ParseError, 'str2bool(): Unknown type of boolean given')
+    end
+
+    return result
+  end
+end
+
+# vim: set ts=2 sw=2 et :
