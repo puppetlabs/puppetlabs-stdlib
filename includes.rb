ab82eceba56077578418a57fe86503b135018ac3
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-04-29 21:02:17 +0100

    Renaming from includes to member in order to avoid confusion and possibility of a clash with Puppet built-in function include.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- includes.rb
+++ includes.rb
@@ -1,27 +1,27 @@
 #
-# include.rb
+# member.rb
 #
 
 # TODO(Krzysztof Wilczynski): We need to add support for regular expression ...
 # TODO(Krzysztof Wilczynski): Support for strings and hashes too ...
 
 module Puppet::Parser::Functions
-  newfunction(:includes, :type => :rvalue, :doc => <<-EOS
+  newfunction(:member, :type => :rvalue, :doc => <<-EOS
     EOS
   ) do |arguments|
 
-    raise(Puppet::ParseError, "includes(): Wrong number of arguments " +
+    raise(Puppet::ParseError, "member(): Wrong number of arguments " +
       "given (#{arguments.size} for 2)") if arguments.size < 2
 
     array = arguments[0]
 
     if not array.is_a?(Array)
-      raise(Puppet::ParseError, 'includes(): Requires an array to work with')
+      raise(Puppet::ParseError, 'member(): Requires an array to work with')
     end
 
     item = arguments[1]
 
-    raise(Puppet::ParseError, 'includes(): You must provide item ' +
+    raise(Puppet::ParseError, 'member(): You must provide item ' +
       'to search for within given array') if item.empty?
 
     result = array.include?(item)
