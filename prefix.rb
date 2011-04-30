b26d5b2f3be95737ea2df63ffccea5354d37d81e
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-04-30 02:40:04 +0100

    Now prefix will convert everything into string which is the same as join would do.  Also function is now more robust in error detection.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- prefix.rb
+++ prefix.rb
@@ -19,11 +19,20 @@ module Puppet::Parser::Functions
 
     prefix = arguments[1] if arguments[1]
 
-    result = array.collect { |i| prefix ? prefix + i : i }
+    if prefix
+      unless prefix.is_a?(String)
+        raise(Puppet::ParseError, 'prefix(): Requires string to work with')
+      end
+    end
+
+    # Turn everything into string same as join would do ...
+    result = array.collect do |i|
+      i = i.to_s
+      prefix ? prefix + i : i
+    end
 
     return result
   end
 end
 
 # vim: set ts=2 sw=2 et :
-
