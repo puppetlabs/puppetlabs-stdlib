f74ab047cd397f16f57e8a3516d5ef7cafcc24d7
Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>
2011-04-30 02:57:48 +0100

    Change boolean detecion from string to make entire function more robust.
    Signed-off-by: Krzysztof Wilczynski <krzysztof.wilczynski@linux.com>

--- bool2num.rb
+++ bool2num.rb
@@ -20,25 +20,23 @@ module Puppet::Parser::Functions
     end
 
     if value.is_a?(String)
-
       # We consider all the yes, no, y, n and so on too ...
-      result = case value
+      value = case value
         #
         # This is how undef looks like in Puppet ...
         # We yield 0 (or false if you wish) in this case.
         #
-        when /^$/, '' then 0 # Empty string will be false ...
-        when /^(1|t|y|true|yes)$/  then 1
-        when /^(0|f|n|false|no)$/  then 0
-        when /^(undef|undefined)$/ then 0 # This is not likely to happen ...
+        when /^$/, '' then false # Empty string will be false ...
+        when /^(1|t|y|true|yes)$/  then true
+        when /^(0|f|n|false|no)$/  then false
+        when /^(undef|undefined)$/ then false # This is not likely to happen ...
         else
           raise(Puppet::ParseError, 'bool2num(): Unknown type of boolean given')
       end
-
-    else
-      # We have real boolean values as well ...
-      result = value ? 1 : 0
     end
+
+    # We have real boolean values as well ...
+    result = value ? 1 : 0
 
     return result
   end
