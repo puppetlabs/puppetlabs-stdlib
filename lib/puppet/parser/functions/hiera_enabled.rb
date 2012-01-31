a78cdf21b1789122481561012b008d5b701baaa3
Garrett Honeycutt <garrett@puppetlabs.com>
2012-01-31 15:46:33 -0800

    (#12345) hiera enabled function for stdlib
    removing extraneous code that was commented out

--- lib/puppet/parser/functions/hiera_enabled.rb
+++ lib/puppet/parser/functions/hiera_enabled.rb
@@ -1,12 +1,5 @@
 module Puppet::Parser::Functions
   newfunction(:hiera_enabled, :type => :rvalue ) do
-    #    begin
-    #      require 'hiera'
-    #      true if Puppet::Parser::Functions.function 'hiera'
-    #    rescue LoadError => e
-    #      false
-    #    end
-    #true if Puppet.features.hiera?
     if Puppet.features.hiera? and Puppet::Parser::Functions.function(:hiera)
       true
     else
