773cb47b3ab9f07302c1d9f057e3f7937cae17a6
Garrett Honeycutt <garrett@puppetlabs.com>
2012-01-31 17:27:01 -0800

    (#12345) added inline doc to hiera_enabled function

--- lib/puppet/parser/functions/hiera_enabled.rb
+++ lib/puppet/parser/functions/hiera_enabled.rb
@@ -1,5 +1,8 @@
 module Puppet::Parser::Functions
-  newfunction(:hiera_enabled, :type => :rvalue ) do
+  newfunction(:hiera_enabled, :type => :rvalue, :doc => <<-EOS
+    Returns true or false based on hiera being enabled.
+    EOS
+ ) do
     if Puppet.features.hiera? and Puppet::Parser::Functions.function(:hiera)
       true
     else
