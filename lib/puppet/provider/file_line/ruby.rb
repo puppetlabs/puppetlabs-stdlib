fc5cfc8cca2d9650d0b7e1dcafe92bf69aedadf9
Peter Meier <peter.meier@immerda.ch>
2012-02-09 15:56:09 +0100

    implement #11017 - make file_line type ensurable
    * Implement a simple destroy method.
    * Add tests for it
    * Refactor code, so file is actually read only once. However, due
      to the nature how provider tests are run, we need to ensure that
      the file is read before we open it to write it.

--- lib/puppet/provider/file_line/ruby.rb
+++ lib/puppet/provider/file_line/ruby.rb
@@ -1,7 +1,7 @@
 Puppet::Type.type(:file_line).provide(:ruby) do
 
   def exists?
-    File.readlines(resource[:path]).find do |line|
+    lines.find do |line|
       line.chomp == resource[:line].chomp
     end
   end
@@ -10,6 +10,18 @@ Puppet::Type.type(:file_line).provide(:ruby) do
     File.open(resource[:path], 'a') do |fh|
       fh.puts resource[:line]
     end
+  end
+
+  def destroy
+    local_lines = lines
+    File.open(resource[:path],'w') do |fh|
+      fh.write(local_lines.reject{|l| l.chomp == resource[:line] }.join(''))
+    end
+  end
+
+  private
+  def lines
+    @lines ||= File.readlines(resource[:path])
   end
 
 end
