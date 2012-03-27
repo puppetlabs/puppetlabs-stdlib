ff7de88561046b4b5b358b2f8a12ca9a71a9b5ea
Wil Cooley <wcooley@nakedape.cc>
2012-03-26 20:53:54 -0700

    Implements has_ip_address predicate

--- lib/puppet/parser/functions/has_ip_address.rb
+++ lib/puppet/parser/functions/has_ip_address.rb
@@ -6,12 +6,21 @@ module Puppet::Parser::Functions
   newfunction(:has_ip_address, :type => :rvalue, :doc => <<-EOS
 Returns true if the client has the requested IP address on some interface.
     EOS
-  ) do |arguments|
+  ) do |args|
 
-    addr = args[0]
+    raise(Pupper::ParseError, "has_ip_address(): Wrong number of arguments " +
+          "given (#{args.size} for 1)") if args.size < 1
 
-    
+    query_addr = args[0]
+    result = false
 
+    lookupvar('interfaces').split(',').each do |iface|
+      if query_addr == lookupvar("ipaddress_#{iface}")
+        result = true
+        break
+      end
+    end
+    result
   end
 end
 
