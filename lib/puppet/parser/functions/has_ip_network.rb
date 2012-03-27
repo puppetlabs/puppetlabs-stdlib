c63696853cf477fe3f1266db6319722520ce17df
Wil Cooley <wcooley@nakedape.cc>
2012-03-26 22:35:09 -0700

    reimplements has_ip_address w/has_interface_with and adds has_ip_network

--- lib/puppet/parser/functions/has_ip_network.rb
+++ lib/puppet/parser/functions/has_ip_network.rb
@@ -1,20 +1,20 @@
 #
-# has_ip_address
+# has_ip_network
 #
 
 module Puppet::Parser::Functions
-  newfunction(:has_ip_address, :type => :rvalue, :doc => <<-EOS
-Returns true if the client has the requested IP address on some interface.
+  newfunction(:has_ip_network, :type => :rvalue, :doc => <<-EOS
+Returns true if the client has an IP address within the requested network.
     EOS
   ) do |args|
 
-    raise(Puppet::ParseError, "has_ip_address(): Wrong number of arguments " +
+    raise(Puppet::ParseError, "has_ip_network(): Wrong number of arguments " +
           "given (#{args.size} for 1)") if args.size != 1
 
     Puppet::Parser::Functions.autoloader.load(:has_interface_with) \
       unless Puppet::Parser::Functions.autoloader.loaded?(:has_interface_with)
 
-    function_has_interface_with(['ipaddress', args[0]])
+    function_has_interface_with(['network', args[0]])
 
   end
 end
