09322ad94398d69081aae44cc770830ac2144b75
Patrick Carlisle <patrick@puppetlabs.com>
2012-08-09 14:18:30 -0700

    Use rvalue functions correctly
    A bug fix in Puppet exposed that several tests were using rvalue functions
    incorrectly (this was not properly checked by puppet before). This fixes those
    tests.

--- spec/unit/puppet/parser/functions/has_key_spec.rb
+++ spec/unit/puppet/parser/functions/has_key_spec.rb
@@ -6,22 +6,32 @@ describe Puppet::Parser::Functions.function(:has_key) do
 
   describe 'when calling has_key from puppet' do
     it "should not compile when no arguments are passed" do
-      Puppet[:code] = 'has_key()'
-      expect { scope.compiler.compile }.should raise_error(Puppet::ParseError, /wrong number of arguments/)
+      Puppet[:code] = '$x = has_key()'
+      expect {
+        scope.compiler.compile
+      }.to raise_error(Puppet::ParseError, /wrong number of arguments/)
     end
+
     it "should not compile when 1 argument is passed" do
-      Puppet[:code] = "has_key('foo')"
-      expect { scope.compiler.compile }.should raise_error(Puppet::ParseError, /wrong number of arguments/)
+      Puppet[:code] = "$x = has_key('foo')"
+      expect {
+        scope.compiler.compile
+      }.to raise_error(Puppet::ParseError, /wrong number of arguments/)
     end
+
     it "should require the first value to be a Hash" do
-      Puppet[:code] = "has_key('foo', 'bar')"
-      expect { scope.compiler.compile }.should raise_error(Puppet::ParseError, /expects the first argument to be a hash/)
+      Puppet[:code] = "$x = has_key('foo', 'bar')"
+      expect {
+        scope.compiler.compile
+      }.to raise_error(Puppet::ParseError, /expects the first argument to be a hash/)
     end
   end
+
   describe 'when calling the function has_key from a scope instance' do
     it 'should detect existing keys' do
       scope.function_has_key([{'one' => 1}, 'one']).should be_true
     end
+
     it 'should detect existing keys' do
       scope.function_has_key([{'one' => 1}, 'two']).should be_false
     end
