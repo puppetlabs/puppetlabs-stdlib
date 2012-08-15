5d99cdf09c08f3161435ffdd2a2b666106386979
Dan Bode <dan@puppetlabs.com>
2012-08-15 15:57:45 -0700

    Explicitly load functions used by ensure_resource
    The ensure_resource function actually calls two
    other functions, create_resources and defined_with_param.
    
    When calling Puppet functions from Ruby, you sometimes have
    to load the functions manually if they have not been called
    before.
    
    This commit explicitly loads the functions that ensure_resource
    depends on from within the function.

--- lib/puppet/parser/functions/ensure_resource.rb
+++ lib/puppet/parser/functions/ensure_resource.rb
@@ -25,9 +25,11 @@ ENDOFDOC
   raise(ArgumentError, 'Must specify a type') unless type
   raise(ArgumentError, 'Must specify a title') unless title
   params ||= {}
+  Puppet::Parser::Functions.function(:defined_with_params)
   if function_defined_with_params(["#{type}[#{title}]", params])
     Puppet.debug("Resource #{type}[#{title}] not created b/c it already exists")
   else
+    Puppet::Parser::Functions.function(:create_resources)
     function_create_resources([type.capitalize, { title => params }])
   end
 end
