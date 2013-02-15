a45d4f13070cd05c029b96c789f499133721d523
Jeff McCune <jeff@puppetlabs.com>
2013-02-15 11:56:16 -0800

    Revert "Merge pull request #130 from jhoblitt/has_element"
    This reverts commit f7a18189ec338b01b0fc89d75def832753af3868, reversing
    changes made to 36a7b29630a4d4de17af79b5dd4e9491ec20b123.
    
    I'm reverting this change because of concerns raised by Peter Meier that
    it duplicates the "in" operator in the DSL.  The "in" operator is new
    information that I did not posses when I made the decision to merge.
    Because of this new information I'm un-merging and continuing the
    discussion in the comments of
    https://projects.puppetlabs.com/issues/19272
    
    Reference: GH-130

--- tests/has_element.pp
+++
@@ -1,9 +0,0 @@
-include stdlib
-
-$my_array = ['key_one']
-if has_element($my_array, 'key_two') {
-  notice('we will not reach here')
-}
-if has_element($my_array, 'key_one') {
-  notice('this will be printed')
-}
