e08734af5a38cb9ad4bc2323104a1a8a76019bb8
Alex Cline <acline@us.ibm.com>
2013-05-03 15:47:27 -0400

    (#20548) Allow an array of resource titles to be passed into the ensure_resource function
    This patch allows an array of resource titles to be passed into
    the ensure_resource function.  Each item in the array will be
    checked for existence and will be created if it doesn't already
    exist.

--- spec/functions/ensure_resource_spec.rb
+++ spec/functions/ensure_resource_spec.rb
@@ -37,4 +37,39 @@ describe 'ensure_resource' do
                             ).and_raise_error(Puppet::Error)
     end
   end
+
+  describe 'when an array of new resources are passed in' do
+    it do
+      should run.with_params('User', ['dan', 'alex'], {})
+      compiler.catalog.resource('User[dan]').to_s.should == 'User[dan]'
+      compiler.catalog.resource('User[alex]').to_s.should == 'User[alex]'
+    end
+  end
+
+  describe 'when an array of existing resources is compared against existing resources' do
+    let :pre_condition do
+      'user { "dan": ensure => present; "alex": ensure => present }'
+    end
+    it do
+      should run.with_params('User', ['dan', 'alex'], {})
+      compiler.catalog.resource('User[dan]').to_s.should == 'User[dan]'
+      compiler.catalog.resource('User[alex]').to_s.should == 'User[alex]'
+    end
+  end
+
+  describe 'when compared against existing resources with attributes' do
+    let :pre_condition do
+      'user { "dan": ensure => present; "alex": ensure => present }'
+    end
+    it do
+      # These should not fail
+      should run.with_params('User', ['dan', 'alex'], {})
+      should run.with_params('User', ['dan', 'alex'], '')
+      should run.with_params('User', ['dan', 'alex'], {'ensure' => 'present'})
+      # This should fail
+      should run.with_params('User', ['dan', 'alex'],
+                             {'ensure' => 'absent'}
+                            ).and_raise_error(Puppet::Error)
+    end
+  end
 end
