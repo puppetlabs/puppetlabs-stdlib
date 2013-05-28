1fcb854f155cd0d63f88a9c792e60555ddffcc16
Adrien Thebo <git@somethingsinistral.net>
2013-05-28 11:11:50 -0700

    (maint) split up range spec with describe blocks

--- spec/unit/puppet/parser/functions/range_spec.rb
+++ spec/unit/puppet/parser/functions/range_spec.rb
@@ -12,54 +12,59 @@ describe "the range function" do
     lambda { scope.function_range([]) }.should( raise_error(Puppet::ParseError))
   end
 
-  it "should return a letter range" do
-    result = scope.function_range(["a","d"])
-    result.should(eq(['a','b','c','d']))
+  describe 'with a letter range' do
+    it "should return a letter range" do
+      result = scope.function_range(["a","d"])
+      result.should(eq(['a','b','c','d']))
+    end
+
+    it "should return a letter range given a step of 1" do
+      result = scope.function_range(["a","d","1"])
+      result.should(eq(['a','b','c','d']))
+    end
+
+    it "should return a stepped letter range" do
+      result = scope.function_range(["a","d","2"])
+      result.should(eq(['a','c']))
+    end
+
+    it "should return a stepped letter range given a negative step" do
+      result = scope.function_range(["a","d","-2"])
+      result.should(eq(['a','c']))
+    end
   end
 
-  it "should return a letter range given a step of 1" do
-    result = scope.function_range(["a","d","1"])
-    result.should(eq(['a','b','c','d']))
+  describe 'with a number range' do
+    it "should return a number range" do
+      result = scope.function_range(["1","4"])
+      result.should(eq([1,2,3,4]))
+    end
+
+    it "should return a number range given a step of 1" do
+      result = scope.function_range(["1","4","1"])
+      result.should(eq([1,2,3,4]))
+    end
+
+    it "should return a stepped number range" do
+      result = scope.function_range(["1","4","2"])
+      result.should(eq([1,3]))
+    end
+
+    it "should return a stepped number range given a negative step" do
+      result = scope.function_range(["1","4","-2"])
+      result.should(eq([1,3]))
+    end
   end
 
-  it "should return a stepped letter range" do
-    result = scope.function_range(["a","d","2"])
-    result.should(eq(['a','c']))
-  end
+  describe 'with a numeric-like string range' do
+    it "should work with padded hostname like strings" do
+      expected = ("host01".."host10").to_a
+      scope.function_range(["host01","host10"]).should eq expected
+    end
 
-  it "should return a stepped letter range given a negative step" do
-    result = scope.function_range(["a","d","-2"])
-    result.should(eq(['a','c']))
+    it "should coerce zero padded digits to integers" do
+      expected = (0..10).to_a
+      scope.function_range(["00", "10"]).should eq expected
+    end
   end
-
-  it "should return a number range" do
-    result = scope.function_range(["1","4"])
-    result.should(eq([1,2,3,4]))
-  end
-
-  it "should work with padded hostname like strings" do
-    expected = ("host01".."host10").to_a
-    scope.function_range(["host01","host10"]).should eq expected
-  end
-
-  it "should coerce zero padded digits to integers" do
-    expected = (0..10).to_a
-    scope.function_range(["00", "10"]).should eq expected
-  end
-
-  it "should return a number range given a step of 1" do
-    result = scope.function_range(["1","4","1"])
-    result.should(eq([1,2,3,4]))
-  end
-
-  it "should return a stepped number range" do
-    result = scope.function_range(["1","4","2"])
-    result.should(eq([1,3]))
-  end
-
-  it "should return a stepped number range given a negative step" do
-    result = scope.function_range(["1","4","-2"])
-    result.should(eq([1,3]))
-  end
-
 end
