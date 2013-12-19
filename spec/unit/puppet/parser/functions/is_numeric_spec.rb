264dc9bbdee8f04b78f8193d62d17b2ba096d671
Simon Effenberg <savar@schuldeigen.de>
2013-12-20 00:13:39 +0100

    (PUP-1195) Fix is_numeric/is_integer when checking non-string parameters
    I expect a function called "is_numeric" or "is_integer" to check if a
    variable is an integer or a number even if the variable passed by isn't
    a string nor a number at all. Otherwise we should call them
    is_string_a_number and is_string_an_integer and we have then to remove
    the check for .is_a?(Number) and .is_a?(FixNum)
    
    now checking also if it is a hex or octal number
    
    improved/corrected checking for integer
    
    * checking against Integer instead of Fixnum so that
      also Bignum is matching
    * now .is_a? Integer is done first so this is quiet fast
    
    Now many types of numerics are recognized.
    
    1. Float/Integer values (signed or unsigned, with exponent or without)
    2. octal and hex check
    3. except hex numbers and the "0." in a float lower than 1 can be prefixed
       with a '0'.
    
    whitespaces shouldn't be allowed as prefix/suffix
    
    string representation of numbers should not contain any type of
    whitespace.. the user is responsible to clean a string before checking
    it..
    
    fix documentation and added more checks
    
    tried to be 99.9% backward compatible
    
    * for now the decission is post poned if hex and octal numbers
      should be allowed or not (is_numeric)
    * native Bignum is now also a valid integer class
    
    fix problem with old 1.8 ruby and Hash.to_s/Array.to_s
    
    In ruby < 1.9 array and hashes would be recognized as numeric
    if they have a special format:
    
    1.8:
    
      [1,2,3,4].to_s = "1234"
      {1=>2}.to_s    = "12"
    
    1.9:
    
      [1,2,3,4].to_s = "[1, 2, 3, 4]"
      {1=>2}.to_s    = "{1=>2}"

--- spec/unit/puppet/parser/functions/is_numeric_spec.rb
+++ spec/unit/puppet/parser/functions/is_numeric_spec.rb
@@ -36,4 +36,84 @@ describe "the is_numeric function" do
     result = scope.function_is_numeric(["asdf"])
     result.should(eq(false))
   end
+
+  it "should return false if an array" do
+    result = scope.function_is_numeric([["asdf"]])
+    result.should(eq(false))
+  end
+
+  it "should return false if an array of integers" do
+    result = scope.function_is_numeric([[1,2,3,4]])
+    result.should(eq(false))
+  end
+
+  it "should return false if a hash" do
+    result = scope.function_is_numeric([{"asdf" => false}])
+    result.should(eq(false))
+  end
+
+  it "should return false if a hash with numbers in it" do
+    result = scope.function_is_numeric([{1 => 2}])
+    result.should(eq(false))
+  end
+
+  it "should return false if a boolean" do
+    result = scope.function_is_numeric([true])
+    result.should(eq(false))
+  end
+
+  it "should return true if a negative float with exponent" do
+    result = scope.function_is_numeric(["-342.2315e-12"])
+    result.should(eq(true))
+  end
+
+  it "should return false if a negative integer with whitespaces before/after the dash" do
+    result = scope.function_is_numeric([" -  751"])
+    result.should(eq(false))
+  end
+
+#  it "should return true if a hexadecimal" do
+#    result = scope.function_is_numeric(["0x52F8c"])
+#    result.should(eq(true))
+#  end
+#
+#  it "should return true if a hexadecimal with uppercase 0X prefix" do
+#    result = scope.function_is_numeric(["0X52F8c"])
+#    result.should(eq(true))
+#  end
+#
+#  it "should return false if a hexadecimal without a prefix" do
+#    result = scope.function_is_numeric(["52F8c"])
+#    result.should(eq(false))
+#  end
+#
+#  it "should return true if a octal" do
+#    result = scope.function_is_numeric(["0751"])
+#    result.should(eq(true))
+#  end
+#
+#  it "should return true if a negative hexadecimal" do
+#    result = scope.function_is_numeric(["-0x52F8c"])
+#    result.should(eq(true))
+#  end
+#
+#  it "should return true if a negative octal" do
+#    result = scope.function_is_numeric(["-0751"])
+#    result.should(eq(true))
+#  end
+#
+#  it "should return false if a negative octal with whitespaces before/after the dash" do
+#    result = scope.function_is_numeric([" -  0751"])
+#    result.should(eq(false))
+#  end
+#
+#  it "should return false if a bad hexadecimal" do
+#    result = scope.function_is_numeric(["0x23d7g"])
+#    result.should(eq(false))
+#  end
+#
+#  it "should return false if a bad octal" do
+#    result = scope.function_is_numeric(["0287"])
+#    result.should(eq(false))
+#  end
 end
