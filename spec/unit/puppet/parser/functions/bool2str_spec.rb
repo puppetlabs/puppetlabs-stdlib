0761fcf0433b1c73ff9925d1b8fa20a618f28875
Ryan McKern <ryan.mckern@puppetlabs.com>
2014-05-13 15:33:49 -0700

    (maint) Add bool2str & camelcase spec tests

---
+++ spec/unit/puppet/parser/functions/bool2str_spec.rb
@@ -0,0 +1,34 @@
+#! /usr/bin/env ruby -S rspec
+require 'spec_helper'
+
+describe "the bool2str function" do
+  let(:scope) { PuppetlabsSpec::PuppetInternals.scope }
+
+  it "should exist" do
+    Puppet::Parser::Functions.function("bool2str").should == "function_bool2str"
+  end
+
+  it "should raise a ParseError if there is less than 1 arguments" do
+    lambda { scope.function_bool2str([]) }.should( raise_error(Puppet::ParseError))
+  end
+
+  it "should convert true to 'true'" do
+    result = scope.function_bool2str([true])
+    result.should(eq('true'))
+  end
+
+  it "should convert true to a string" do
+    result = scope.function_bool2str([true])
+    result.class.should(eq(String))
+  end
+
+  it "should convert false to 'false'" do
+    result = scope.function_bool2str([false])
+    result.should(eq('false'))
+  end
+
+  it "should convert false to a string" do
+    result = scope.function_bool2str([false])
+    result.class.should(eq(String))
+  end
+end
