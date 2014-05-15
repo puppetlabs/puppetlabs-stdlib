c5b06f9bbca7acc491560c92a73d7e2a153fe0a7
Ashley Penney <ashley.penney@puppetlabs.com>
2014-05-15 17:28:59 -0400

    Revert "Merge pull request #256 from stbenjam/2571-before"
    This reverts commit 8499ebdb7f892f2623295058649c67a5553d4732, reversing
    changes made to 08b00d9229961d7b3c3cba997bfb35c8d47e4c4b.

--- lib/puppet/type/file_line.rb
+++ lib/puppet/type/file_line.rb
@@ -46,10 +46,6 @@ Puppet::Type.newtype(:file_line) do
     desc 'An optional value used to specify the line after which we will add any new lines. (Existing lines are added in place)'
   end
 
-  newparam(:before) do
-    desc 'An optional value used to specify the line before which we will add any new lines. (Existing lines are added in place)'
-  end
-
   newparam(:line) do
     desc 'The line to be appended to the file located by the path parameter.'
   end
