b4014ad89cb473d1d135cc7c0b1907110f3a02b0
Jérôme <jerome.riviere974@gmail.com>
2014-06-04 10:09:01 +0200

    Add spec
    Added spec for file_exists function
    Added require puppet

--- lib/puppet/parser/functions/file_exists.rb
+++ lib/puppet/parser/functions/file_exists.rb
@@ -2,9 +2,13 @@
 # Simple function that check if a file exist or not
 #
 # Created by Jerome RIVIERE (www.jerome-riviere.re) (https://github.com/ninja-2)
+require "puppet"
 
 module Puppet::Parser::Functions
-  newfunction(:file_exists, :type => :rvalue) do |args|
+  newfunction(:file_exists, :type => :rvalue, :doc => <<-EOS
+Returns true if the file exists.
+    EOS
+  ) do |args|
     if File.exists?(args[0])
       return true
     else
