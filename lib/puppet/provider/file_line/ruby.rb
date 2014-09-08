66fe62a6aad3cfef7b78bac719b1390895e3991b
Aleksandr Didenko <adidenko@mirantis.com>
2014-09-08 14:34:25 +0300

    Add support for using both match and after
    Previously we were running "match" handler ignoring "after" param.
    This patch enables us to use both "match" and "after" resource
    parameters: if "match" is found, then we simply replace the line,
    if it's not found and we have "after", then we add lines after the
    specified pattern. If "match" and "after" not found, then we just
    append the line.

--- lib/puppet/provider/file_line/ruby.rb
+++ lib/puppet/provider/file_line/ruby.rb
@@ -38,13 +38,17 @@ Puppet::Type.type(:file_line).provide(:ruby) do
     if match_count > 1 && resource[:multiple].to_s != 'true'
      raise Puppet::Error, "More than one line in file '#{resource[:path]}' matches pattern '#{resource[:match]}'"
     end
-    File.open(resource[:path], 'w') do |fh|
-      lines.each do |l|
-        fh.puts(regex.match(l) ? resource[:line] : l)
-      end
+    if (match_count == 0) and resource[:after]
+      handle_create_with_after
+    elsif
+      File.open(resource[:path], 'w') do |fh|
+        lines.each do |l|
+          fh.puts(regex.match(l) ? resource[:line] : l)
+        end
 
-      if (match_count == 0)
-        fh.puts(resource[:line])
+        if (match_count == 0)
+          fh.puts(resource[:line])
+        end
       end
     end
   end
