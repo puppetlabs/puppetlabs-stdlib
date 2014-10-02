25bff13ec7f5bba83855d1ba22276a84b2d71ee2
David Quattlebaum <sasdrq@sas.com>
2014-10-01 21:27:58 -0400

    Use the function arity feature to remove a lot of boilerplate code from the functions.

--- lib/puppet/parser/functions/basename.rb
+++ lib/puppet/parser/functions/basename.rb
@@ -1,11 +1,8 @@
 module Puppet::Parser::Functions
-  newfunction(:basename, :type => :rvalue, :doc => <<-EOS
+  newfunction(:basename, :type => :rvalue, :arity => 1, doc => <<-EOS
     Returns the basename of a path.
     EOS
   ) do |arguments|
-
-    raise(Puppet::ParseError, "basename(): Wrong number of arguments " +
-      "given (#{arguments.size} for 1)") if arguments.size < 1
 
     path = arguments[0]
     return File.basename(path)
