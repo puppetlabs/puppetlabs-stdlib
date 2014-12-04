5e49c504580bf06353c841c51f1319a5bda893a8
Peter Souter <p.souter@kainos.com>
2014-12-04 14:15:33 +0000

    (MODULES-444) Acceptance for multiple arrays
    Acceptance test to take multiple arrays for concatenation

--- spec/acceptance/concat_spec.rb
+++ spec/acceptance/concat_spec.rb
@@ -25,5 +25,16 @@ describe 'concat function', :unless => UNSUPPORTED_PLATFORMS.include?(fact('oper
 
       apply_manifest(pp, :catch_failures => true)
     end
+    it 'should concat multiple arrays to one' do
+      pp = <<-EOS
+      $output = concat(['1','2','3'],['4','5','6'],['7','8','9'])
+      validate_array($output)
+      if size($output) != 6 {
+        fail("${output} should have 9 elements.")
+      }
+      EOS
+
+      apply_manifest(pp, :catch_failures => true)
+    end
   end
 end
