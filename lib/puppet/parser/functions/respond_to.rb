ebcb06bc82e9122a402efe67fe1da59020793eb0
David Danzilio <ddanzilio@constantcontact.com>
2015-04-21 00:01:12 -0400

    Let respond_to accept an array of parameters.

--- lib/puppet/parser/functions/respond_to.rb
+++ lib/puppet/parser/functions/respond_to.rb
@@ -8,23 +8,34 @@ returns true if that resource responds to the given parameter.
     respond_to('class', 'apt', 'always_update_apt')
     respond_to('file', 'path')
     respond_to('apt::key', 'key')
+    respond_to('archive', ['checksum_type', 'checksum_file'])
 EOS
 ) do |args|
   args = [args] unless args.is_a?(Array)
+  valid_params = []
 
   if args[0] =~ /^(?i:class)$/
-    type, title, param = args
+    type, title, params = args
     raise ArgumentError, 'Must specify a class name' unless title
-    raise ArgumentError, 'Must specify a parameter' unless param
+    raise ArgumentError, 'Must specify a parameter' unless params
     resource = find_hostclass(title)
   else
-    title, param = args
+    title, params = args
     type = 'resource'
-    raise ArgumentError, 'Must specify a parameter' unless param
+    raise ArgumentError, 'Must specify a parameter' unless params
     resource = find_resource_type(title) || find_definition(title)
   end
 
   raise ArgumentError, "The #{title} #{type} could not be found" if resource.nil?
 
-  resource.valid_parameter?(param)
+  params = [params] unless params.is_a?(Array)
+  params.each do |param|
+    valid_params << resource.valid_parameter?(param)
+  end
+
+  if valid_params.include?(false)
+    return false
+  else
+    return true
+  end
 end
