1d9189d860f28067b72093cbe4027cf49b7d612c
Jetroid <holtjet@gmail.com>
2015-08-24 12:01:29 +0100

    (MODULE-2456) Modify union to accept more than two arrays
    Add spec tests to test the new functionality:
     *Case for 3 arrays.
     *Case for 4 arrays.
    Modify README to note new functionality.
    
    This is for issue MODULE-2456, follow the precedent of MODULE-444.
    
    This change allows union to be much more useful, unioning many arrays
    in one line rather than in n lines. Additionally, as this is only added
    functionality, and does not affect the 2 array case that all modules
    currently using array are using, it should not affect any existing
    modules utilizing union.
    
    This is now useful, for example, for merging many arrays of resources
    (eg: packages.) to generate just one list with no duplicates, to avoid
    duplicate resource declarations.

--- spec/functions/union_spec.rb
+++ spec/functions/union_spec.rb
@@ -5,10 +5,9 @@ describe 'union' do
     it { is_expected.not_to eq(nil) }
     it { is_expected.to run.with_params().and_raise_error(Puppet::ParseError, /wrong number of arguments/i) }
     it { is_expected.to run.with_params('one').and_raise_error(Puppet::ParseError, /wrong number of arguments/i) }
-    it { is_expected.to run.with_params('one', 'two', 'three').and_raise_error(Puppet::ParseError, /wrong number of arguments/i) }
-    it { is_expected.to run.with_params('one', []).and_raise_error(Puppet::ParseError, /Requires 2 arrays/) }
-    it { is_expected.to run.with_params([], 'two').and_raise_error(Puppet::ParseError, /Requires 2 arrays/) }
-    it { is_expected.to run.with_params({}, {}).and_raise_error(Puppet::ParseError, /Requires 2 arrays/) }
+    it { is_expected.to run.with_params('one', []).and_raise_error(Puppet::ParseError, /Every parameter must be an array/) }
+    it { is_expected.to run.with_params([], 'two').and_raise_error(Puppet::ParseError, /Every parameter must be an array/) }
+    it { is_expected.to run.with_params({}, {}).and_raise_error(Puppet::ParseError, /Every parameter must be an array/) }
   end
 
   it { is_expected.to run.with_params([], []).and_return([]) }
@@ -19,5 +18,7 @@ describe 'union' do
   it { is_expected.to run.with_params(['one', 'two', 'three'], ['two', 'three']).and_return(['one', 'two', 'three']) }
   it { is_expected.to run.with_params(['one', 'two', 'two', 'three'], ['two', 'three']).and_return(['one', 'two', 'three']) }
   it { is_expected.to run.with_params(['one', 'two', 'three'], ['two', 'two', 'three']).and_return(['one', 'two', 'three']) }
+  it { is_expected.to run.with_params(['one', 'two'], ['two', 'three'], ['one', 'three']).and_return(['one', 'two', 'three']) }
+  it { is_expected.to run.with_params(['one', 'two'], ['three', 'four'], ['one', 'two', 'three'], ['four']).and_return(['one', 'two', 'three', 'four']) }
   it 'should not confuse types' do is_expected.to run.with_params(['1', '2', '3'], [1, 2]).and_return(['1', '2', '3', 1, 2]) end
 end
