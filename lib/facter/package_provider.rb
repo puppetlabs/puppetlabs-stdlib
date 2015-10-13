d73fd3c5efcdf9a60178ddf96914ab75c9214260
Martin Pfeifer <martin.pfeifer@dm.de>
2015-10-13 10:08:01 +0200

    prevent deprecation warning about the allow_virtual parameter

--- lib/facter/package_provider.rb
+++ lib/facter/package_provider.rb
@@ -12,6 +12,10 @@ require 'puppet/type/package'
 
 Facter.add(:package_provider) do
   setcode do
-    Puppet::Type.type(:package).newpackage(:name => 'dummy')[:provider].to_s
+    if Gem::Version.new(Facter.value(:puppetversion)) >= Gem::Version.new('3.6')
+      Puppet::Type.type(:package).newpackage(:name => 'dummy', :allow_virtual => 'true')[:provider].to_s
+    else
+      Puppet::Type.type(:package).newpackage(:name => 'dummy')[:provider].to_s
+    end
   end
 end
