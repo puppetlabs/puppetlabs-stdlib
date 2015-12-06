c12e9c08201cea34851908b69ed3486e5fe33585
Sanjeev <Sanjeev.Nithyanandam@cdk.com>
2015-12-05 21:13:07 -0600

    Revert unintended change

--- lib/facter/package_provider.rb
+++ lib/facter/package_provider.rb
@@ -13,7 +13,7 @@ require 'puppet/type/package'
 Facter.add(:package_provider) do
   setcode do
     puppetversion = Facter.value(:pe_version) || Facter.value(:puppetversion)
-    if Gem::Version.new(puppetversion) > Gem::Version.new('3.6')
+    if Gem::Version.new(puppetversion) >= Gem::Version.new('3.6')
       Puppet::Type.type(:package).newpackage(:name => 'dummy', :allow_virtual => 'true')[:provider].to_s
     else
       Puppet::Type.type(:package).newpackage(:name => 'dummy')[:provider].to_s
