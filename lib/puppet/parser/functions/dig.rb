06d2016e4d2f8a2a2ba3e6812404d8af12231ea8
Maksym Melnychok <maksym@yelp.com>
2016-02-08 07:50:35 -0800

    add dig function

---
+++ lib/puppet/parser/functions/dig.rb
@@ -0,0 +1,33 @@
+#
+# dig.rb
+#
+
+module Puppet::Parser::Functions
+  newfunction(:dig, :type => :rvalue, :doc => <<-EOS
+Deep lookup in nested hash. Returns nil or provided
+default value if path can't be looked up.
+    EOS
+             ) do |arguments|
+    # Two arguments are required
+    raise(Puppet::ParseError, "difference(): Wrong number of arguments " +
+                              "given (#{arguments.size} for at least 2)") if arguments.size < 2
+
+    hash, path, default = *arguments
+
+    unless hash.is_a? Hash
+      raise(Puppet::ParseError, "first argument must be hash, " <<
+                                "given #{hash.class.name}")
+    end
+
+    unless path.is_a? Array
+      raise(Puppet::ParseError, "first argument must be an array, " <<
+                                "given #{path.class.name}")
+    end
+
+    path.each { |key| hash = hash[key] rescue nil }
+
+    hash || default
+  end
+end
+
+# vim: set ts=2 sw=2 et :
---
+++ spec/functions/dig_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'dig' do
+  it { is_expected.to run.with_params().and_raise_error(Puppet::ParseError) }
+  it { is_expected.to run.with_params('bad', []).and_raise_error(Puppet::ParseError) }
+  it { is_expected.to run.with_params({}, 'bad').and_raise_error(Puppet::ParseError) }
+
+  it { is_expected.to run.with_params({}, []).and_return({}) }
+  it { is_expected.to run.with_params({"a" => "b"}, ["a"]).and_return("b") }
+  it { is_expected.to run.with_params({"a" => {"b" => "c"}}, ["a", "b"]).and_return("c") }
+  it { is_expected.to run.with_params({}, ["a", "b"], "d").and_return("d") }
+end
