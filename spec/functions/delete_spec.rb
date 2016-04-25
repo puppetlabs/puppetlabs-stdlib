232de137f1018060b256b1f3f649be0b6d7d9952
Hunter Haugen <hunter@puppet.com>
2016-04-25 14:33:43 -0700

    Revert "Add support for regular expressions to delete"
    This reverts commit 0d46515b57cea60d4d5f1e4d81a75a448a7a73a8.
    
    It introduced backwards-incompatible functionality.

--- spec/functions/delete_spec.rb
+++ spec/functions/delete_spec.rb
@@ -12,7 +12,6 @@ describe 'delete' do
     it { is_expected.to run.with_params([], 'two').and_return([]) }
     it { is_expected.to run.with_params(['two'], 'two').and_return([]) }
     it { is_expected.to run.with_params(['two', 'two'], 'two').and_return([]) }
-    it { is_expected.to run.with_params(['one', 'two', 'three'], '^t.*').and_return(['one']) }
     it { is_expected.to run.with_params(['one', 'two', 'three'], 'four').and_return(['one', 'two', 'three']) }
     it { is_expected.to run.with_params(['one', 'two', 'three'], 'two').and_return(['one', 'three']) }
     it { is_expected.to run.with_params(['two', 'one', 'two', 'three', 'two'], 'two').and_return(['one', 'three']) }
@@ -33,7 +32,7 @@ describe 'delete' do
     it { is_expected.to run.with_params('barfoobar', ['foo', 'barbar']).and_return('') }
   end
 
-  describe 'deleting from a hash' do
+  describe 'deleting from an array' do
     it { is_expected.to run.with_params({}, '').and_return({}) }
     it { is_expected.to run.with_params({}, 'key').and_return({}) }
     it { is_expected.to run.with_params({'key' => 'value'}, 'key').and_return({}) }
@@ -44,10 +43,6 @@ describe 'delete' do
     it { is_expected.to run \
       .with_params({'key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'}, ['key1', 'key2']) \
       .and_return( {'key3' => 'value3'})
-    }
-    it { is_expected.to run \
-      .with_params({'key1' => 'value1', 'key2' => 'value2', 'key3' => 'value3'}, ['^key\d']) \
-      .and_return({})
     }
   end
 
