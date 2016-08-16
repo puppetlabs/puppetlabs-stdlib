e9e1ed6da511d5dc9a1e7623885cdb41ed81aaaf
Sander Cornelissen <Cornelissen.S@tkppensioen.nl>
2016-08-16 17:23:09 +0200

    Make sure the resource is marked as not exist when there are more found

--- lib/puppet/provider/file_line/ruby.rb
+++ lib/puppet/provider/file_line/ruby.rb
@@ -2,6 +2,8 @@ Puppet::Type.type(:file_line).provide(:ruby) do
   def exists?
     if resource[:replace].to_s != 'true' and count_matches(match_regex) > 0
       true
+    elsif resource[:replace].to_s != 'false' and count_matches(match_regex) > 1 and resource[:multiple].to_s != 'false'
+      false
     else
       lines.find do |line|
         line.chomp == resource[:line].chomp
