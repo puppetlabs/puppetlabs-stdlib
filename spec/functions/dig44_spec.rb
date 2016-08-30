789263cab5ca2f38face3399076f4d3be34b3d3d
Dmitry Ilyin <di476k@att.com>
2016-08-30 15:12:29 -0500

    Refactor dig44 function
    The current implementation of the dig44 function has the
    following problems:
    * Doesn't recognise Puppet4's :undef value as an empty value
      and doesn't return the default value.
    * Doesn't make a different between false and nil value
      and returns the default value for a non-empty false value

--- spec/functions/dig44_spec.rb
+++ spec/functions/dig44_spec.rb
@@ -1,44 +1,105 @@
 require 'spec_helper'
 
 describe 'dig44' do
-  it "should exist" do
-    expect(Puppet::Parser::Functions.function("dig44")).to eq("function_dig44")
+
+  let(:data) do
+    {
+        'a' => {
+            'g' => '2',
+            'e' => [
+                'f0',
+                'f1',
+                {
+                    'x' => {
+                        'y' => 'z'
+                    }
+                },
+                'f3',
+            ]
+        },
+        'b' => true,
+        'c' => false,
+        'd' => '1',
+        'e' => :undef,
+        'f' => nil,
+    }
   end
 
-  it "should raise a ParseError if there are less than 2 arguments" do
-    expect { scope.function_dig44([]) }.to raise_error(Puppet::ParseError)
-  end 
+  context 'single values' do
+    it 'should exist' do
+      is_expected.not_to be_nil
+    end
 
-  it "should raise a ParseError if the first argument isn't a hash or array" do
-    expect { scope.function_dig44(['bad', []]) }.to raise_error(Puppet::ParseError)
-  end 
+    it 'should require two arguments' do
+      is_expected.to run.with_params().and_raise_error(ArgumentError)
+    end
 
-  it "should raise a ParseError if the second argument isn't an array" do
-    expect { scope.function_dig44([{}, 'bad']) }.to raise_error(Puppet::ParseError)
+    it 'should fail if the data is not a structure' do
+      is_expected.to run.with_params('test', []).and_raise_error(Puppet::Error)
+    end
+
+    it 'should fail if the path is not an array' do
+      is_expected.to run.with_params({}, '').and_raise_error(Puppet::Error)
+    end
+
+    it 'should return the value if the value is string' do
+      is_expected.to run.with_params(data, ['d'], 'default').and_return('1')
+    end
+
+    it 'should return true if the value is true' do
+      is_expected.to run.with_params(data, ['b'], 'default').and_return(true)
+    end
+
+    it 'should return false if the value is false' do
+      is_expected.to run.with_params(data, ['c'], 'default').and_return(false)
+    end
+
+    it 'should return the default if the value is nil' do
+      is_expected.to run.with_params(data, ['f'], 'default').and_return('default')
+    end
+
+    it 'should return the default if the value is :undef (same as nil)' do
+      is_expected.to run.with_params(data, ['e'], 'default').and_return('default')
+    end
+
+    it 'should return the default if the path is not found' do
+      is_expected.to run.with_params(data, ['missing'], 'default').and_return('default')
+    end
   end
 
-  it "should return an empty hash when given empty parameters" do
-    result = scope.function_dig44([{}, []])
-    expect(result).to(eq({}))
-  end
+  context 'structure values' do
 
-  it "should return value when given simple hash" do
-    result = scope.function_dig44([{"a" => "b"}, ["a"]])
-    expect(result).to(eq("b"))
-  end
+    it 'should be able to extract a deeply nested hash value' do
+      is_expected.to run.with_params(data, %w(a g), 'default').and_return('2')
+    end
 
-  it "should find hash values two levels deep" do
-    result = scope.function_dig44([{"a" => {"b" => "c"}}, ["a", "b"]])
-    expect(result).to(eq("c"))
-  end
+    it 'should return the default value if the path is too long' do
+      is_expected.to run.with_params(data, %w(a g c d), 'default').and_return('default')
+    end
 
-  it "should return default value if nothing was found" do
-    result = scope.function_dig44([{}, ["a", "b"], "d"])
-    expect(result).to(eq("d"))
-  end
+    it 'should support an array index (number) in the path' do
+      is_expected.to run.with_params(data, ['a', 'e', 1], 'default').and_return('f1')
+    end
 
-  it "should work on booleans as well as strings" do
-    result = scope.function_dig44([{"a" => false}, ["a"]])
-    expect(result).to(eq(false))
+    it 'should support an array index (string) in the path' do
+      is_expected.to run.with_params(data, %w(a e 1), 'default').and_return('f1')
+    end
+
+    it 'should return the default value if an array index is not a number' do
+      is_expected.to run.with_params(data, %w(a b c), 'default').and_return('default')
+    end
+
+    it 'should return the default value if and index is out of array length' do
+      is_expected.to run.with_params(data, %w(a e 5), 'default').and_return('default')
+    end
+
+    it 'should be able to path though both arrays and hashes' do
+      is_expected.to run.with_params(data, %w(a e 2 x y), 'default').and_return('z')
+    end
+
+    it 'should return "nil" if value is not found and no default value is provided' do
+      is_expected.to run.with_params(data, %w(a 1)).and_return(nil)
+    end
+
   end
 end
