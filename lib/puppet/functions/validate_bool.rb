69c69e750bdaee7d5eec264650551b149538de92
Helen <helen@puppetlabs.com>
2016-09-21 16:22:37 +0100

    Revert "Ensure validate functions use Puppet 4 deprecation"

--- lib/puppet/functions/validate_bool.rb
+++
@@ -1,3 +0,0 @@
-require 'puppet_x/puppetlabs/stdlib/deprecation_gen'
-PuppetX::Puppetlabs::Stdlib.deprecation_gen("validate_bool", "Stdlib::Compat::Bool")
-# Puppet::Functions.create_function
--- lib/puppet/functions/validate_integer.rb
+++
@@ -1,3 +0,0 @@
-require 'puppet_x/puppetlabs/stdlib/deprecation_gen'
-PuppetX::Puppetlabs::Stdlib.deprecation_gen("validate_integer", "Stdlib::Compat::Integer")
-# Puppet::Functions.create_function
