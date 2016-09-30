16a7c1b406400125e8041c87e5bc8d8c976fcbb9
Maksym Melnychok <maksym@yelp.com>
2016-09-30 08:45:22 -0700

    move dig to dig44 because puppet 4

--- lib/puppet/parser/functions/dig44.rb
+++ lib/puppet/parser/functions/dig44.rb
@@ -1,26 +1,26 @@
 #
-# dig.rb
+# dig44.rb
 #
 
 module Puppet::Parser::Functions
-  newfunction(:dig, :type => :rvalue, :doc => <<-EOS
+  newfunction(:dig44, :type => :rvalue, :doc => <<-EOS
 Deep lookup in nested hash. Returns nil or provided
 default value if path can't be looked up.
     EOS
              ) do |arguments|
     # Two arguments are required
-    raise(Puppet::ParseError, "dig(): Wrong number of arguments " +
+    raise(Puppet::ParseError, "dig44(): Wrong number of arguments " +
                               "given (#{arguments.size} for at least 2)") if arguments.size < 2
 
     hash, path, default = *arguments
 
     unless hash.is_a? Hash
-      raise(Puppet::ParseError, "dig(): first argument must be hash, " <<
+      raise(Puppet::ParseError, "dig44(): first argument must be hash, " <<
                                 "given #{hash.class.name}")
     end
 
     unless path.is_a? Array
-      raise(Puppet::ParseError, "dig(): second argument must be an array, " <<
+      raise(Puppet::ParseError, "dig44(): second argument must be an array, " <<
                                 "given #{path.class.name}")
     end
 
