a936439a0ad27e2de2bf7ad7786530ae3ebf37b8
Daniele Sluijters <daenney@users.noreply.github.com>
2016-10-01 16:34:40 +0200

    Make the IPv4|6 regex much more strict
    This will correctly match IPv4 and IPv6 addresses and discard invalid
    ones.

--- types/compat/ipv4.pp
+++ types/compat/ipv4.pp
@@ -1,2 +1,2 @@
 # Emulate the validate_ipv4_address and is_ipv4_address functions
-type Stdlib::Compat::Ipv4 = Pattern[/^(\d+)\.(\d+)\.(\d+)\.(\d+)$/]
+type Stdlib::Compat::Ipv4 = Pattern[/^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/]
