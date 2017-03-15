2c138deb4fa9c1d9e1e7b1daa506d85eb3aa1666
Helen Campbell <helen@puppetlabs.com>
2017-03-15 11:13:45 +0000

    (FM-6051) Adds comments to warn for UTF8 incompatibility

--- lib/puppet/parser/functions/uriescape.rb
+++ lib/puppet/parser/functions/uriescape.rb
@@ -1,5 +1,6 @@
 #
 #  uriescape.rb
+#  Please note: This function is an implementation of a Ruby class and as such may not be entirely UTF8 compatible. To ensure compatibility please use this function with Ruby 2.4.0 or greater - https://bugs.ruby-lang.org/issues/10085.
 #
 require 'uri'
 
