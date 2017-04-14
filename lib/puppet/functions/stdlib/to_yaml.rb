2fdd4d404d8761750af3f6a2c258f5b517bf16a0
Nick Walker <nick.walker@puppetlabs.com>
2017-04-14 10:48:36 -0700

    Add a stdlib::to_yaml function

---
+++ lib/puppet/functions/stdlib/to_yaml.rb
@@ -0,0 +1,14 @@
+require 'yaml'
+
+Puppet::Functions.create_function(:'stdlib::to_yaml') do
+  dispatch :to_yaml do
+    param 'Hash', :hash_or_array
+  end
+  dispatch :to_yaml do
+    param 'Array', :hash_or_array
+  end
+
+  def to_yaml(hash_or_array)
+    hash_or_array.to_yaml
+  end
+end
