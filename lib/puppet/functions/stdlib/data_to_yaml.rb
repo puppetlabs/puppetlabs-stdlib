c730176e164b3610984f9dff52e54e125e1f2483
Nick Walker <nick.walker@puppetlabs.com>
2017-06-05 13:47:11 -0700

    Validate that the parameter is of type Data
    Prior to this commit, we made sure the parameter was a hash or
    an Array.
    
    After this commit, we make sure the parameter is of type Data.

--- lib/puppet/functions/stdlib/data_to_yaml.rb
+++ lib/puppet/functions/stdlib/data_to_yaml.rb
@@ -2,10 +2,7 @@ require 'yaml'
 
 Puppet::Functions.create_function(:'stdlib::data_to_yaml') do
   dispatch :to_yaml do
-    param 'Hash', :hash_or_array
-  end
-  dispatch :to_yaml do
-    param 'Array', :hash_or_array
+    param 'Data', :hash_or_array
   end
 
   def to_yaml(hash_or_array)
