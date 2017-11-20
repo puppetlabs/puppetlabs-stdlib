3169d9a90816b0922debc167fa20a45ce88aec8c
Paula McMaw <paula@puppet.com>
2017-11-20 11:03:04 +0000

    Update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,6 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 # put local configuration and setup into spec_helper_local
 begin
   require 'spec_helper_local'
-rescue LoadError => loaderror # rubocop:disable Lint/HandleExceptions
+rescue LoadError => loaderror
   puts "Could not require spec_helper_local: #{loaderror.message}"
 end
