2936cf11e68e9af93ad2b19b7d13ca473b8c809e
Eugene Piven <thepegasd@gmail.com>
2017-11-28 10:02:01 +0300

    RuboCop fix

--- spec/functions/to_json_pretty_spec.rb
+++ spec/functions/to_json_pretty_spec.rb
@@ -11,6 +11,6 @@ describe 'to_json_pretty' do
     is_expected.to run.with_params('one' => { 'oneA' => 'A', 'oneB' => { 'oneB1' => '1', 'oneB2' => '2' } }, 'two' => %w[twoA twoB])
                       .and_return("{\n  \"one\": {\n    \"oneA\": \"A\",\n    \"oneB\": {\n      \"oneB1\": \"1\",\n      \"oneB2\": \"2\"\n    }\n  },\n  \"two\": [\n    \"twoA\",\n    \"twoB\"\n  ]\n}\n") # rubocop:disable Metrics/LineLength : Unable to reduce line to required length
   }
-  it { is_expected.to run.with_params({'one' => '1', 'two' => nil}, true).and_return("{\n  \"one\": \"1\"\n}\n") }
+  it { is_expected.to run.with_params({ 'one' => '1', 'two' => nil }, true).and_return("{\n  \"one\": \"1\"\n}\n") }
   it { is_expected.to run.with_params(['one', 'two', nil, 'three'], true).and_return("[\n  \"one\",\n  \"two\",\n  \"three\"\n]\n") }
 end
