655883500cefadc1ce866121510e968b71c53591
c33s <dev@c33s.net>
2017-12-05 19:15:25 +0100

    fixed wrong comment in unixpath.pp

--- types/unixpath.pp
+++ types/unixpath.pp
@@ -1,2 +1,2 @@
-# this regex rejects any path component that is a / or a NUL
+# this regex rejects any path component that does not start with "/", contain a "." or is NUL
 type Stdlib::Unixpath = Pattern[/^\/([^\/\0]+\/*)*$/]
