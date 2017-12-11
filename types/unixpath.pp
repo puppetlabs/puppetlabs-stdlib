c95ae34f225b402a91a83bf822832f3d10c21fd4
Julian <dev@c33s.net>
2017-12-11 12:36:58 +0100

    fixed wrong comment in unixpath.pp (#862)
    * fixed wrong comment in unixpath.pp
    * removed the comment about "."

--- types/unixpath.pp
+++ types/unixpath.pp
@@ -1,2 +1,2 @@
-# this regex rejects any path component that is a / or a NUL
+# this regex rejects any path component that does not start with "/" or is NUL
 type Stdlib::Unixpath = Pattern[/^\/([^\/\0]+\/*)*$/]
