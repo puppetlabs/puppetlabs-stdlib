3f1ea8d88798d777c64737a8b078046df408b92a
Alexander Olofsson <alexander.olofsson@liu.se>
2018-04-24 14:52:58 +0200

    (MODULES-7024) Add 20-octet MAC addresses

--- types/mac.pp
+++ types/mac.pp
@@ -1,2 +1,5 @@
 # A type for a MAC address
-type Stdlib::MAC = Pattern[/^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/]
+type Stdlib::MAC = Pattern[
+  /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/,
+  /^([0-9A-Fa-f]{2}[:-]){19}([0-9A-Fa-f]{2})$/
+]
