f3bf4cd031c352a7f5dbb6a10d9d5a6df2cce2cf
Henrik Lindberg <henrik.lindberg@puppetlabs.com>
2018-05-14 15:03:12 +0200

    (PUP-8603) Add note that abs() is in puppet since 6.0.0

--- lib/puppet/parser/functions/abs.rb
+++ lib/puppet/parser/functions/abs.rb
@@ -5,6 +5,9 @@ module Puppet::Parser::Functions
   newfunction(:abs, :type => :rvalue, :doc => <<-DOC
     Returns the absolute value of a number, for example -34.56 becomes
     34.56. Takes a single integer and float value as an argument.
+
+    Note: from Puppet 6.0.0, the compatible function with the same name in Puppet core
+    will be used instead of this function.
     DOC
              ) do |arguments|
 
