6f3d984eaf2fda4275f3f1c3d8361c05f019ca9a
Jonathan Tripathy <jt@puppet.com>
2018-06-12 16:40:03 +0200

    Add the ability for the loadyaml() function to accept an HTTP or HTTPS URI.

--- lib/puppet/parser/functions/loadyaml.rb
+++ lib/puppet/parser/functions/loadyaml.rb
@@ -11,21 +11,25 @@ module Puppet::Parser::Functions
     For example:
 
         $myhash = loadyaml('/etc/puppet/data/myhash.yaml')
+        $myhash = loadyaml('https://example.local/my_hash.yaml')
         $myhash = loadyaml('no-file.yaml', {'default' => 'value'})
   DOC
 
     raise ArgumentError, 'Wrong number of arguments. 1 or 2 arguments should be provided.' unless args.length >= 1
     require 'yaml'
-
-    if File.exists?(args[0]) # rubocop:disable Lint/DeprecatedClassMethods : Changing to .exist? breaks the code
-      begin
+    require 'open-uri'
+    begin
+      if args[0].start_with?('http://') || args[0].start_with?('https://')
+        contents = open(args[0])
+        YAML.load(contents) || args[1]
+      elsif File.exists?(args[0]) # rubocop:disable Lint/DeprecatedClassMethods : Changing to .exist? breaks the code
         YAML.load_file(args[0]) || args[1]
-      rescue StandardError => e
-        raise e unless args[1]
+      else
+        warning("Can't load '#{args[0]}' File does not exist!")
         args[1]
       end
-    else
-      warning("Can't load '#{args[0]}' File does not exist!")
+    rescue StandardError => e
+      raise e unless args[1]
       args[1]
     end
   end
