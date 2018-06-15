32637f450216d50b2cb71033af68ac92e883b809
Henrik Lindberg <563066+hlindberg@users.noreply.github.com>
2018-06-15 17:06:52 +0200

    (PUP-8491) Skip tests for `ceiling` if puppet version < 6.0.0

--- spec/acceptance/ceiling_spec.rb
+++ spec/acceptance/ceiling_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper_acceptance'
 
-describe 'ceiling function' do
+describe 'ceiling function', :if => Puppet::Util::Package.versioncmp(Puppet.version, '6.0.0') < 0 do
   describe 'success' do
     pp1 = <<-DOC
       $a = 12.8
