302b8e9cafcbc884760269130ef7938177934c64
Henrik Lindberg <563066+hlindberg@users.noreply.github.com>
2018-09-25 21:48:36 +0200

    (FM-7388) Fix pick_default_spec assumptions about undef
    Before this, the pick_default_spec would fail on Puppet 6.0.0
    because on Puppet 6.0.0 arguments given to a function are mapped
    to comply with the 3x function calling spec.
    
    The tests asserted that :undef and nil were returned as values (when
    they were given as arguments) - however this never happens when the
    function is called from the Puppet Language. (This the test was testing
    assumptions that were not true).
    
    The spec test is now modified so that when it runs for Puppet 6.0.0 and
    later it will do the same mapping on the value as done on the input of
    that value to the function before it compares the returned value against
    the input. Thus retaining the spirit of the test.

--- spec/functions/pick_default_spec.rb
+++ spec/functions/pick_default_spec.rb
@@ -17,10 +17,23 @@ describe 'pick_default' do
       it { is_expected.to run.with_params('ớņệ', value).and_return('ớņệ') }
       it { is_expected.to run.with_params([], value).and_return([]) }
       it { is_expected.to run.with_params({}, value).and_return({}) }
-      it { is_expected.to run.with_params(value, value).and_return(value) }
-      it { is_expected.to run.with_params(:undef, value).and_return(value) }
-      it { is_expected.to run.with_params(:undefined, value).and_return(value) }
-      it { is_expected.to run.with_params(nil, value).and_return(value) }
+      it { is_expected.to run.with_params(value, value).and_return(mapped_value(value)) }
+      it { is_expected.to run.with_params(:undef, value).and_return(mapped_value(value)) }
+      it { is_expected.to run.with_params(:undefined, value).and_return(mapped_value(value)) }
+      it { is_expected.to run.with_params(nil, value).and_return(mapped_value(value)) }
+    end
+  end
+
+  if Puppet::Util::Package.versioncmp(Puppet.version, '6.0.0') < 0
+    def mapped_value(v) 
+      v
+    end
+  else
+    def mapped_value(v)
+      # Puppet 6.0.0 will always map arguments the same way as the Puppet Language
+      # even if function is called from Ruby via call_function
+      # The 3x function API expects nil and :undef to be represented as empty string
+      return v.nil? || v == :undef ? '' : v
     end
   end
 end
