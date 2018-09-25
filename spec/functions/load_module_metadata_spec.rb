53ac9585dbdb58fe475e3df243450ff998d219b6
Henrik Lindberg <563066+hlindberg@users.noreply.github.com>
2018-09-25 22:52:09 +0200

    (FM-7388) Fix load_module_metadata_spec for Puppet 6.0.0
    Before this the test would fail because it used `subject.call` which
    hits a compatibility method in rspec_puppet that expects to be called
    with `scope`.
    
    This is now fixed by instead using `subject.execute`

--- spec/functions/load_module_metadata_spec.rb
+++ spec/functions/load_module_metadata_spec.rb
@@ -26,7 +26,7 @@ describe 'load_module_metadata' do
         allow(File).to receive(:exists?).with("#{prefix}/path/to/module/metadata.json").and_return(true)
         allow(File).to receive(:read).with("#{prefix}/path/to/module/metadata.json").and_return('{"name": "spencer-science"}')
 
-        result = subject.call(['science'])
+        result = subject.execute('science')
         expect(result['name']).to eq('spencer-science')
       end
 
@@ -39,7 +39,7 @@ describe 'load_module_metadata' do
       it 'returns nil if user allows empty metadata.json' do
         allow(scope).to receive(:function_get_module_path).with(['science']).and_return("#{prefix}/path/to/module/")
         allow(File).to receive(:exists?).with("#{prefix}/path/to/module/metadata.json").and_return(false)
-        result = subject.call(['science', true])
+        result = subject.execute('science', true)
         expect(result).to eq({})
       end
     end
