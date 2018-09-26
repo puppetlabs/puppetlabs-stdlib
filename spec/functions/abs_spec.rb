8ba212cee35bd4de819fe2e3c9256f29b75ee23e
Henrik Lindberg <563066+hlindberg@users.noreply.github.com>
2018-09-26 20:34:37 +0200

    (FM-7388) Update abs_spec to not fail on Puppet < 6.0.0

--- spec/functions/abs_spec.rb
+++ spec/functions/abs_spec.rb
@@ -12,6 +12,10 @@ describe 'abs', :if => Puppet::Util::Package.versioncmp(Puppet.version, '6.0.0')
   end
 
   describe 'signature validation in puppet4', :if => RSpec.configuration.puppet_future do
+    before(:each) do
+      pending('does not raise error on versions before Puppet 6.0.0') if Puppet::Util::Package.versioncmp(Puppet.version, '6.0.0') < 0
+    end
+
     it { is_expected.to run.with_params.and_raise_error(ArgumentError) }
     it { is_expected.to run.with_params(1, 2).and_raise_error(ArgumentError) }
     it { is_expected.to run.with_params([]).and_raise_error(ArgumentError) }
