c350865d601e8a5518dbdb9a351895cfcf54b38f
Eimhin Laverty <eimhin.laverty@puppet.com>
2019-02-12 13:06:49 +0000

    Test refactor for downcase function

--- spec/functions/downcase_spec.rb
+++ spec/functions/downcase_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'downcase', :if => Puppet::Util::Package.versioncmp(Puppet.version, '6.0.0') < 0 do
   it { is_expected.not_to eq(nil) }
-  it { is_expected.to run.with_params.and_raise_error(Puppet::ParseError) }
-  it { is_expected.to run.with_params(100).and_raise_error(Puppet::ParseError) }
+  it { is_expected.to run.with_params.and_raise_error(Puppet::ParseError, %r{Wrong number of arguments}) }
+  it { is_expected.to run.with_params(100).and_raise_error(Puppet::ParseError, %r{Requires either array or string}) }
   it { is_expected.to run.with_params('abc').and_return('abc') }
   it { is_expected.to run.with_params('Abc').and_return('abc') }
   it { is_expected.to run.with_params('ABC').and_return('abc') }
