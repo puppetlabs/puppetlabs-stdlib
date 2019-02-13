87b1771779c5a0ec2317ae575443a81fda40a322
Paula Muir <paula@puppet.com>
2019-02-13 09:57:34 +0000

    Test refactor for is_ipv6_address function

--- spec/functions/is_ipv6_address_spec.rb
+++ spec/functions/is_ipv6_address_spec.rb
@@ -9,6 +9,7 @@ describe 'is_ipv6_address' do
   it { is_expected.to run.with_params('1.2.3.4.5').and_return(false) }
   it { is_expected.to run.with_params('').and_return(false) }
   it { is_expected.to run.with_params('one').and_return(false) }
+  it { is_expected.to run.with_params('2001:0db8:85a3:0000:0000:8a2e:0370:7334:ggg').and_return(false) }
 
   context 'Checking for deprecation warning', :if => Puppet.version.to_f < 4.0 do
     after(:each) do
