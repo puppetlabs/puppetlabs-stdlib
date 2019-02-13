67204689fccefa1efa7058c5e382c25e37fe6b34
Eimhin Laverty <eimhin.laverty@puppet.com>
2019-02-13 13:52:22 +0000

    Fix dirname unit failure on Puppet 6

--- spec/functions/dirname_spec.rb
+++ spec/functions/dirname_spec.rb
@@ -8,8 +8,8 @@ describe 'dirname' do
   it { is_expected.to run.with_params({}).and_raise_error(Puppet::ParseError, %r{Requires string as argument}) }
   it { is_expected.to run.with_params(1).and_raise_error(Puppet::ParseError, %r{Requires string as argument}) }
   it { is_expected.to run.with_params('').and_raise_error(Puppet::ParseError, %r{Requires a non-empty string as argument}) }
-  it { is_expected.to run.with_params(:undef).and_raise_error(Puppet::ParseError, %r{Requires string as argument}) }
-  it { is_expected.to run.with_params(nil).and_raise_error(Puppet::ParseError, %r{Requires string as argument}) }
+  it { is_expected.to run.with_params(:undef).and_raise_error(Puppet::ParseError, %r{string as argument}) }
+  it { is_expected.to run.with_params(nil).and_raise_error(Puppet::ParseError, %r{string as argument}) }
   it { is_expected.to run.with_params('/path/to/a/file.ext').and_return('/path/to/a') }
   it { is_expected.to run.with_params('relative_path/to/a/file.ext').and_return('relative_path/to/a') }
 
