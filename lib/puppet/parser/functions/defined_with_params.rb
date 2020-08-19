cf7b3023d4a3348e37181ebe807deba97a598fcd
Trevor Vaughan <tvaughan@onyxpoint.com>
2020-08-19 17:35:57 -0400

    [MODULES-10781] Fix defined type defined_with_params()
    An edge case was missed previously where calling the
    defined_with_params() function from inside a defined type on another
    instance of the same defined type would always return a match because it
    would match against itself.
    
    This updates the code to ignore itself and also adds a debug line output
    for any resource that was matched.
    
    MODULES-10781 #close

--- lib/puppet/parser/functions/defined_with_params.rb
+++ lib/puppet/parser/functions/defined_with_params.rb
@@ -54,12 +54,19 @@ DOC
                 [findresource(type, title)]
               end
 
-  resources.compact.each do |resource|
+  resources.compact.each do |res|
+    # If you call this from within a defined type, it will find itself
+    next if res.to_s == resource.to_s
+
     matches = params.map do |key, value|
       # eql? avoids bugs caused by monkeypatching in puppet
-      resource_is_undef = resource[key].eql?(:undef) || resource[key].nil?
+      res_is_undef = res[key].eql?(:undef) || res[key].nil?
       value_is_undef = value.eql?(:undef) || value.nil?
-      (resource_is_undef && value_is_undef) || (resource[key] == value)
+      found_match = (res_is_undef && value_is_undef) || (res[key] == value)
+
+      Puppet.debug("Matching resource is #{res}") if found_match
+
+      found_match
     end
     ret = params.empty? || !matches.include?(false)
 
