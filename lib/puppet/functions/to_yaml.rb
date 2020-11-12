fcd00999fa193088e0d825ba794c904e2cf0002b
Matthias Baur <m.baur@syseleven.de>
2020-11-12 12:26:35 +0100

    Allow options injection for to_yaml
    This makes it possible to inject formatting options into the to_yaml
    function.

--- lib/puppet/functions/to_yaml.rb
+++ lib/puppet/functions/to_yaml.rb
@@ -2,21 +2,28 @@ require 'yaml'
 # @summary
 #   Convert a data structure and output it as YAML
 #
-# @example how to output YAML
+# @example How to output YAML
 #   # output yaml to a file
 #     file { '/tmp/my.yaml':
 #       ensure  => file,
 #       content => to_yaml($myhash),
 #     }
+# @example Use options control the output format
+#   file { '/tmp/my.yaml':
+#     ensure  => file,
+#     content => to_yaml($myhash, {indentation: 4})
+#   }
 Puppet::Functions.create_function(:to_yaml) do
   # @param data
+  # @param options
   #
   # @return [String]
   dispatch :to_yaml do
     param 'Any', :data
+    optional_param 'Hash', :options
   end
 
-  def to_yaml(data)
-    data.to_yaml
+  def to_yaml(data, options = {})
+    data.to_yaml(options)
   end
 end
