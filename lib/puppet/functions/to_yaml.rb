3f94a708b7772d9dbc33deb6d7ba5e26fbc7a304
Alexander Fisher <alex@linfratech.co.uk>
2022-04-13 10:42:21 +0100

    Fix `to_yaml` `options` parameter
    Use Ruby 2.5's `transform_keys` method to symbolize the keys in the
    `options` parameter before passing to Ruby's `to_yaml` function.
    
    Note, this requires Puppet 6 and above for the required Ruby version to
    be available on both the agent (for `puppet apply`) _and_ the server
    (Puppetserver 6 ships with JRuby 9.2.x which is Ruby 2.5 compatible).
    
    Also fixed up the docs of the `to_json` function for better consistency
    between the two functions.

--- lib/puppet/functions/to_yaml.rb
+++ lib/puppet/functions/to_yaml.rb
@@ -4,28 +4,29 @@ require 'yaml'
 # @summary
 #   Convert a data structure and output it as YAML
 #
-# @example How to output YAML
-#   # output yaml to a file
-#     file { '/tmp/my.yaml':
-#       ensure  => file,
-#       content => to_yaml($myhash),
-#     }
-# @example Use options control the output format
+# @example Output YAML to a file
 #   file { '/tmp/my.yaml':
 #     ensure  => file,
-#     content => to_yaml($myhash, {indentation: 4})
+#     content => to_yaml($myhash),
+#   }
+# @example Use options to control the output format
+#   file { '/tmp/my.yaml':
+#     ensure  => file,
+#     content => to_yaml($myhash, {indentation => 4})
 #   }
 Puppet::Functions.create_function(:to_yaml) do
   # @param data
+  #   The data you want to convert to YAML
   # @param options
+  #   A hash of options that will be passed to Ruby's Psych library. Note, this could change between Puppet versions, but at time of writing these are `line_width`, `indentation`, and `canonical`.
   #
-  # @return [String]
+  # @return [String] The YAML document
   dispatch :to_yaml do
     param 'Any', :data
     optional_param 'Hash', :options
   end
 
   def to_yaml(data, options = {})
-    data.to_yaml(options)
+    data.to_yaml(options.transform_keys(&:to_sym))
   end
 end
