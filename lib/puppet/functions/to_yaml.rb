1c5f7dcff90c60338bcecfa53eb4f478707ff565
Romain Tartière <romain@blogreen.org>
2022-04-28 11:07:54 -1000

    Fix examples location
    They must be at the dispatch call.

--- lib/puppet/functions/to_yaml.rb
+++ lib/puppet/functions/to_yaml.rb
@@ -3,22 +3,22 @@
 require 'yaml'
 # @summary
 #   Convert a data structure and output it as YAML
-#
-# @example Output YAML to a file
-#   file { '/tmp/my.yaml':
-#     ensure  => file,
-#     content => to_yaml($myhash),
-#   }
-# @example Use options to control the output format
-#   file { '/tmp/my.yaml':
-#     ensure  => file,
-#     content => to_yaml($myhash, {indentation => 4})
-#   }
 Puppet::Functions.create_function(:to_yaml) do
   # @param data
   #   The data you want to convert to YAML
   # @param options
   #   A hash of options that will be passed to Ruby's Psych library. Note, this could change between Puppet versions, but at time of writing these are `line_width`, `indentation`, and `canonical`.
+  #
+  # @example Output YAML to a file
+  #   file { '/tmp/my.yaml':
+  #     ensure  => file,
+  #     content => to_yaml($myhash),
+  #   }
+  # @example Use options to control the output format
+  #   file { '/tmp/my.yaml':
+  #     ensure  => file,
+  #     content => to_yaml($myhash, {indentation => 4})
+  #   }
   #
   # @return [String] The YAML document
   dispatch :to_yaml do
