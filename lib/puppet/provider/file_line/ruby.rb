2dbe43ceb2e38155889ec806dccdd7bb287af8fc
Steven Pritchard <steve@sicura.us>
2022-06-16 09:28:49 -0500

    Minor coding style change based on feedback

--- lib/puppet/provider/file_line/ruby.rb
+++ lib/puppet/provider/file_line/ruby.rb
@@ -83,8 +83,8 @@ Puppet::Type.type(:file_line).provide(:ruby) do
   rescue TypeError => _e
     # Ruby 1.8 doesn't support open_args
     @lines ||= File.readlines(resource[:path])
-  rescue Errno::ENOENT => _e
-    raise _e unless resource.noop?
+  rescue Errno::ENOENT
+    raise unless resource.noop?
     @lines ||= []
   end
 
