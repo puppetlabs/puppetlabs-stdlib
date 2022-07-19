348a04cb0b8d220e56bfa53c01ba9602d80f76a9
Ben Ford <ben.ford@puppetlabs.com>
2022-07-19 08:33:05 -0700

    rubocop fixes

--- spec/functions/stdlib_deferrable_epp_spec.rb
+++ spec/functions/stdlib_deferrable_epp_spec.rb
@@ -5,21 +5,24 @@ describe 'stdlib::deferrable_epp' do
     let(:pre_condition) do
       'function epp($str, $data) { return "rendered"}'
     end
+
     it {
-      is_expected.to run.with_params('mymod/template.epp', {'foo' => 'bar'}).and_return('rendered')
+      is_expected.to run.with_params('mymod/template.epp', { 'foo' => 'bar' }).and_return('rendered')
     }
   end
 
   context 'defers rendering with deferred input' do
-    let(:pre_condition) do (<<~END)
+    let(:pre_condition) do
+      <<~END
         function epp($str, $data) { fail("should not have invoked epp()") }
         function find_template($str) { return "path" }
         function file($path) { return "foo: <%= foo %>" }
       END
     end
+
     it {
-      foo = Puppet::Pops::Types::TypeFactory.deferred.create('join', [1,2,3])
-      is_expected.to run.with_params('mymod/template.epp', {'foo' => foo }) #.and_return(a_kind_of Puppet::Pops::Evaluator::DeferredValue)
+      foo = Puppet::Pops::Types::TypeFactory.deferred.create('join', [1, 2, 3])
+      is_expected.to run.with_params('mymod/template.epp', { 'foo' => foo }) # .and_return(a_kind_of Puppet::Pops::Evaluator::DeferredValue)
     }
   end
 end
