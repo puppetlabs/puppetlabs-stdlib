f1752d8dc48a6dbc6ca3a7ab555e14a56cce9bd3
david22swan <david.swan@puppet.com>
2022-08-15 14:13:44 +0100

    Use 'require_relative' to load stdlib due to lookup errors

--- lib/puppet_x/stdlib/toml_dumper.rb
+++ lib/puppet_x/stdlib/toml_dumper.rb
@@ -18,7 +18,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-require 'puppet_x/stdlib'
+require_relative '../stdlib'
 require 'date'
 
 module PuppetX::Stdlib
