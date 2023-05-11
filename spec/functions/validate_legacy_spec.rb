1bcbf877a879675b9fa69ebf74676f375cd951c7
Romain Tartière <romain@blogreen.org>
2023-05-11 08:29:58 -1000

    Deprecate the `validate_legacy()` function
    `validate_legacy()` was added to help migrating from the legacy
    validation functions to the new Puppet data types which where not 100%
    backward compatible with the previous validation (e.g. `'42'` was a
    valid integer according to `validate_integer()` but indeed a String and
    not an Integer when using data types.
    
    The legacy validation functions have been removed from stdlib, so
    `validate_legacy()` will now fail if it tries to run them.  But as
    they produced deprecation warning, they are supposed to have already
    been fixed.  For the sake of security, instead of removing
    `validate_legacy()` now, deprecate it so that it is strictly equivalent
    to validating using a data type, but also emits a warning.

--- spec/functions/validate_legacy_spec.rb
+++ spec/functions/validate_legacy_spec.rb
@@ -7,41 +7,28 @@ if Puppet::Util::Package.versioncmp(Puppet.version, '4.4.0') >= 0
     it { is_expected.not_to eq(nil) }
     it { is_expected.to run.with_params.and_raise_error(ArgumentError) }
 
-    describe 'when passing the type assertion and passing the previous validation' do
-      it 'passes without notice' do
-        expect(scope).to receive(:function_validate_foo).with([5]).once
+    describe 'when passing the type assertion' do
+      it 'passes with a deprecation warning' do
+        expect(subject.func).to receive(:call_function).with('deprecation', 'validate_legacy', include('deprecated')).once
+        expect(scope).to receive(:function_validate_foo).never
         expect(Puppet).to receive(:notice).never
         is_expected.to run.with_params('Integer', 'validate_foo', 5)
       end
     end
 
-    describe 'when passing the type assertion and failing the previous validation' do
-      it 'passes with a notice about newly accepted value' do
-        expect(scope).to receive(:function_validate_foo).with([5]).and_raise(Puppet::ParseError, 'foo').once
-        expect(Puppet).to receive(:notice).with(include('Accepting previously invalid value for target type'))
-        is_expected.to run.with_params('Integer', 'validate_foo', 5)
-      end
-    end
-
-    describe 'when failing the type assertion and passing the previous validation' do
-      it 'passes with a deprecation message' do
-        expect(scope).to receive(:function_validate_foo).with(['5']).once
-        expect(subject.func).to receive(:call_function).with('deprecation', 'validate_legacy', include('Integer')).once
-        is_expected.to run.with_params('Integer', 'validate_foo', '5')
-      end
-    end
-
-    describe 'when failing the type assertion and failing the previous validation' do
+    describe 'when failing the type assertion' do
       it 'fails with a helpful message' do
-        expect(scope).to receive(:function_validate_foo).with(['5']).and_raise(Puppet::ParseError, 'foo').once
-        expect(subject.func).to receive(:call_function).with('fail', include('Integer')).once
+        expect(subject.func).to receive(:call_function).with('deprecation', 'validate_legacy', include('deprecated')).once
+        expect(scope).to receive(:function_validate_foo).never
+        expect(subject.func).to receive(:call_function).with('fail', 'validate_legacy(Integer, ...) expects an Integer value, got String').once
         is_expected.to run.with_params('Integer', 'validate_foo', '5')
       end
     end
 
     describe 'when passing in undef' do
       it 'works' do
-        expect(scope).to receive(:function_validate_foo).with([:undef]).once
+        expect(subject.func).to receive(:call_function).with('deprecation', 'validate_legacy', include('deprecated')).once
+        expect(scope).to receive(:function_validate_foo).never
         expect(Puppet).to receive(:notice).never
         is_expected.to run.with_params('Optional[Integer]', 'validate_foo', :undef)
       end
@@ -49,7 +36,8 @@ if Puppet::Util::Package.versioncmp(Puppet.version, '4.4.0') >= 0
 
     describe 'when passing in multiple arguments' do
       it 'passes with a deprecation message' do
-        expect(scope).to receive(:function_validate_foo).with([:undef, 1, 'foo']).once
+        expect(subject.func).to receive(:call_function).with('deprecation', 'validate_legacy', include('deprecated')).once
+        expect(scope).to receive(:function_validate_foo).never
         expect(Puppet).to receive(:notice).never
         is_expected.to run.with_params('Optional[Integer]', 'validate_foo', :undef, 1, 'foo')
       end
