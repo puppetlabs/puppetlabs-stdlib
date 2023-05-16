39c486ba77eaf9e97798b44013714f415caffdb7
Lukas Audzevicius <laudzevicius24@hotmail.com>
2023-05-16 09:22:37 +0100

    Addressing uriescape testing

--- spec/functions/uriescape_spec.rb
+++ spec/functions/uriescape_spec.rb
@@ -3,36 +3,39 @@
 require 'spec_helper'
 
 describe 'uriescape' do
-  describe 'signature validation' do
-    it { is_expected.not_to eq(nil) }
-    it { is_expected.to run.with_params.and_raise_error(Puppet::ParseError, %r{wrong number of arguments}i) }
-    it {
-      pending('Current implementation ignores parameters after the first.')
-      is_expected.to run.with_params('', '').and_raise_error(Puppet::ParseError, %r{wrong number of arguments}i)
-    }
-    it { is_expected.to run.with_params(1).and_raise_error(Puppet::ParseError, %r{Requires either array or string to work}) }
-    it { is_expected.to run.with_params({}).and_raise_error(Puppet::ParseError, %r{Requires either array or string to work}) }
-    it { is_expected.to run.with_params(true).and_raise_error(Puppet::ParseError, %r{Requires either array or string to work}) }
-  end
-
-  describe 'handling normal strings' do
-    it 'calls ruby\'s URI::DEFAULT_PARSER.escape function' do
-      expect(URI::DEFAULT_PARSER).to receive(:escape).with('uri_string').and_return('escaped_uri_string').once
-      is_expected.to run.with_params('uri_string').and_return('escaped_uri_string')
+  # URI.escape has been fully removed as of Ruby 3. Therefore, it will not work as it stand on Puppet 8.
+  if Puppet::Util::Package.versioncmp(Puppet.version, '8.0.0').negative?
+    describe 'signature validation' do
+      it { is_expected.not_to eq(nil) }
+      it { is_expected.to run.with_params.and_raise_error(Puppet::ParseError, %r{wrong number of arguments}i) }
+      it {
+        pending('Current implementation ignores parameters after the first.')
+        is_expected.to run.with_params('', '').and_raise_error(Puppet::ParseError, %r{wrong number of arguments}i)
+      }
+      it { is_expected.to run.with_params(1).and_raise_error(Puppet::ParseError, %r{Requires either array or string to work}) }
+      it { is_expected.to run.with_params({}).and_raise_error(Puppet::ParseError, %r{Requires either array or string to work}) }
+      it { is_expected.to run.with_params(true).and_raise_error(Puppet::ParseError, %r{Requires either array or string to work}) }
     end
-  end
 
-  describe 'handling classes derived from String' do
-    it 'calls ruby\'s URI::DEFAULT_PARSER.escape function' do
-      uri_string = AlsoString.new('uri_string')
-      expect(URI::DEFAULT_PARSER).to receive(:escape).with(uri_string).and_return('escaped_uri_string').once
-      is_expected.to run.with_params(uri_string).and_return('escaped_uri_string')
+    describe 'handling normal strings' do
+      it 'calls ruby\'s URI::DEFAULT_PARSER.escape function' do
+        expect(URI::DEFAULT_PARSER).to receive(:escape).with('uri_string').and_return('escaped_uri_string').once
+        is_expected.to run.with_params('uri_string').and_return('escaped_uri_string')
+      end
     end
-  end
 
-  describe 'strings in arrays handling' do
-    it { is_expected.to run.with_params([]).and_return([]) }
-    it { is_expected.to run.with_params(['one}', 'two']).and_return(['one%7D', 'two']) }
-    it { is_expected.to run.with_params(['one}', 1, true, {}, 'two']).and_return(['one%7D', 1, true, {}, 'two']) }
+    describe 'handling classes derived from String' do
+      it 'calls ruby\'s URI::DEFAULT_PARSER.escape function' do
+        uri_string = AlsoString.new('uri_string')
+        expect(URI::DEFAULT_PARSER).to receive(:escape).with(uri_string).and_return('escaped_uri_string').once
+        is_expected.to run.with_params(uri_string).and_return('escaped_uri_string')
+      end
+    end
+
+    describe 'strings in arrays handling' do
+      it { is_expected.to run.with_params([]).and_return([]) }
+      it { is_expected.to run.with_params(['one}', 'two']).and_return(['one%7D', 'two']) }
+      it { is_expected.to run.with_params(['one}', 1, true, {}, 'two']).and_return(['one%7D', 1, true, {}, 'two']) }
+    end
   end
 end
