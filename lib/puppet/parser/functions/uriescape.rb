35c325de3eb30071ae9f6f88164c18167b79569c
Steve Traylen <steve.traylen@cern.ch>
2023-10-17 16:53:19 +0200

    drop redundant comments

--- lib/puppet/parser/functions/uriescape.rb
+++ lib/puppet/parser/functions/uriescape.rb
@@ -3,8 +3,6 @@
 require 'uri'
 #
 #  uriescape.rb
-#  Please note: This function is an implementation of a Ruby class and as such may not be entirely UTF8 compatible. To ensure compatibility please use this function with Ruby 2.4.0 or greater - https://bugs.ruby-lang.org/issues/10085.
-#  URI.escape has been fully removed as of Ruby 3. Therefore, it will not work as it stand on Puppet 8. Consider deprecating or updating this function.
 #
 module Puppet::Parser::Functions
   newfunction(:uriescape, type: :rvalue, doc: <<-DOC
