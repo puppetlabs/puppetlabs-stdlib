puppetlabs/puppetlabs-stdlib

commit fcbd4267fd830982e86041599d4eb11c2072a94a

Remove deprecated Stdlib::Compat::* data types
These data types where introduced in Puppet 4 when deprecating the
validate_* functions and provided backwards compatibility with older
versions of Puppet where all parameters where String.

With Puppet 4, we got the ability to manage proper data types and it is
now time to remove these compatibility types.

types/host.pp
@@ -1,2 +1,2 @@
 # @summary Validate a host (FQDN or IP address)
-type Stdlib::Host = Variant[Stdlib::Fqdn, Stdlib::Compat::Ip_address]
+type Stdlib::Host = Variant[Stdlib::Fqdn, Stdlib::Ip::Address]

spec/type_aliases/array_spec.rb
@@ -1,32 +1,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-describe 'Stdlib::Compat::Array' do
-  describe 'accepts arrays' do
-    [
-      [],
-      ['one'],
-      [1],
-      [{}],
-      [[]],
-    ].each do |value|
-      describe value.inspect do
-        it { is_expected.to allow_value(value) }
-      end
-    end
-  end
-
-  describe 'rejects other values' do
-    [
-      '',
-      'one',
-      '1',
-      {},
-    ].each do |value|
-      describe value.inspect do
-        it { is_expected.not_to allow_value(value) }
-      end
-    end
-  end
-end

spec/type_aliases/bool_spec.rb
@@ -1,30 +1,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-describe 'Stdlib::Compat::Bool' do
-  describe 'accepts booleans' do
-    [
-      true,
-      false,
-    ].each do |value|
-      describe value.inspect do
-        it { is_expected.to allow_value(value) }
-      end
-    end
-  end
-
-  describe 'rejects other values' do
-    [
-      [1],
-      [{}],
-      [true],
-      'true',
-      'false',
-    ].each do |value|
-      describe value.inspect do
-        it { is_expected.not_to allow_value(value) }
-      end
-    end
-  end
-end

spec/type_aliases/compat__ip_address.rb
@@ -1,38 +1,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-describe 'Stdlib::Compat::Ip_address' do
-  describe 'accepts ipv4 and ipv6 addresses' do
-    [
-      '224.0.0.0',
-      '255.255.255.255',
-      '0.0.0.0',
-      '192.88.99.0',
-      '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
-      'fa76:8765:34ac:0823:ab76:eee9:0987:1111',
-    ].each do |value|
-      describe value.inspect do
-        it { is_expected.to allow_value(value) }
-      end
-    end
-  end
-  describe 'rejects other values' do
-    [
-      nil,
-      [nil],
-      [nil, nil],
-      { 'foo' => 'bar' },
-      {},
-      '',
-      'nope',
-      '77',
-      '4.4.4',
-      '2001:0db8:85a3:000000:0000:8a2e:0370:7334',
-    ].each do |value|
-      describe value.inspect do
-        it { is_expected.not_to allow_value(value) }
-      end
-    end
-  end
-end

spec/type_aliases/compat__ipv4_spec.rb
@@ -1,20 +1,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-describe 'Stdlib::Compat::Ipv4' do
-  describe 'accepts ipv4 addresses' do
-    SharedData::IPV4_PATTERNS.each do |value|
-      describe value.inspect do
-        it { is_expected.to allow_value(value) }
-      end
-    end
-  end
-  describe 'rejects other values' do
-    SharedData::IPV4_NEGATIVE_PATTERNS.each do |value|
-      describe value.inspect do
-        it { is_expected.not_to allow_value(value) }
-      end
-    end
-  end
-end

spec/type_aliases/compat__ipv6_spec.rb
@@ -1,44 +1,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-describe 'Stdlib::Compat::Ipv6' do
-  describe 'accepts ipv6 addresses' do
-    [
-      '2001:0db8:85a3:0000:0000:8a2e:0370:7334',
-      'fa76:8765:34ac:0823:ab76:eee9:0987:1111',
-      'fe80:0000:0000:0000:0204:61ff:fe9d:f156',
-      'fe80:0:0:0:204:61ff:fe9d:f156',
-      'fe80::204:61ff:fe9d:f156',
-      'fe80:0:0:0:0204:61ff:254.157.241.86',
-      '::1',
-      'fe80::',
-      '2001::',
-    ].each do |value|
-      describe value.inspect do
-        it { is_expected.to allow_value(value) }
-      end
-    end
-  end
-  describe 'rejects other values' do
-    [
-      nil,
-      [nil],
-      [nil, nil],
-      { 'foo' => 'bar' },
-      {},
-      '',
-      'nope',
-      '77',
-      '4.4.4',
-      '2000:7334',
-      '::ffff:2.3.4',
-      '::ffff:257.1.2.3',
-      '::ffff:12345678901234567890.1.26',
-    ].each do |value|
-      describe value.inspect do
-        it { is_expected.not_to allow_value(value) }
-      end
-    end
-  end
-end

spec/type_aliases/float_spec.rb
@@ -1,26 +1,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-describe 'Stdlib::Compat::Float' do
-  describe 'accepts floats' do
-    [
-      3.7,
-      '3.7',
-      -3.7,
-      '-342.2315e-12',
-    ].each do |value|
-      describe value.inspect do
-        it { is_expected.to allow_value(value) }
-      end
-    end
-  end
-
-  describe 'rejects other values' do
-    [true, 'true', false, 'false', 'iAmAString', '1test', '1 test', 'test 1', 'test 1 test', {}, { 'key' => 'value' }, { 1 => 2 }, '', :undef, 'x', 3, '3', -3, '-3'].each do |value|
-      describe value.inspect do
-        it { is_expected.not_to allow_value(value) }
-      end
-    end
-  end
-end

spec/type_aliases/hash_spec.rb
@@ -1,30 +1,0 @@
-# frozen_string_literal: true
-
-require 'spec_helper'
-
-describe 'Stdlib::Compat::Hash' do
-  describe 'accepts hashes' do
-    [
-      {},
-      { 'one' => 'two' },
-      { 'wan' => 3 },
-      { '001' => 'helly' },
-    ].each do |value|
-      describe value.inspect do
-        it { is_expected.to allow_value(value) }
-      end
-    end
-  end
-  describe 'rejects other values' do
-    [
-      '',
-      'one',
-      '1',
-      [],
-    ].each do |value|
-      describe value.inspect do
-        it { is_expected.not_to allow_value(value) }
-      end
-    end
-  end
-end

commit bf91cd59ab2143ff5d8511cdbe3b1758ca2aa984

Correct casing of Stdlib::IP::Address
While Puppet doesn't appear to care about IP vs Ip, but Kafo does and
can't find the correct type. This aligns the usage with the actual name
in the file.

Fixes: fcbd4267fd83 ("Remove deprecated Stdlib::Compat::* data types")

types/host.pp
@@ -1,2 +1,2 @@
 # @summary Validate a host (FQDN or IP address)
-type Stdlib::Host = Variant[Stdlib::Fqdn, Stdlib::Ip::Address]
+type Stdlib::Host = Variant[Stdlib::Fqdn, Stdlib::IP::Address]